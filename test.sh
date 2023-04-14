67deb0aac12194a84b09e621fdb4f4589adb0245
Gaizgrol <gabrielizotongo@gmail.com>
2023-04-14 02:47:57 -0300

    test: formato de discovery alterado

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker exec -it api-fastapi sh -c "python -m unittest discover --verbose && exit" && \
+docker exec -it api-fastapi sh -c "python -m unittest discover -p \"*_test.py\" --verbose && exit" && \
 docker exec -it front-nextjs sh -c "printf \"console.log('Start testing')\" | node && exit"
