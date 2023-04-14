2bb20b643aed72e5cc4ac67709fbf529af62510b
Gaizgrol <gabrielizotongo@gmail.com>
2023-04-14 15:54:43 -0300

    tests: correções

--- fastapi/categories/services_test.py
+++ fastapi/categories/services_test.py
@@ -7,10 +7,15 @@ from .repository import CategoryRepository
 class TestCategoryService(TestCase):
     def setUp(self):
         self.category_repository = Mock()
+        self.category_repository.create = Mock(return_value=('Bebida', 5))
         self.category_service = CategoryService(
             category_repository=self.category_repository
         )
 
     def test_category_create_should_store_category(self):
         self.category_service.create('Bebida', 5)
-        self.category_repository.create.assert_called_once_with('Bebida', 5)
+        self.category_repository.create.assert_called_once
+
+    def test_category_create_should_list_all(self):
+        self.category_service.all()
+        self.category_repository.all.assert_called_once
